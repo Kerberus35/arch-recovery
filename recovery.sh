baf9909fa7291c88e76f4f5b66634ed14f4bd098
Jasper <Kerberus35@users.noreply.github.com>
2024-01-24 00:17:54 +0100

    Create recovery.sh

---
+++ recovery.sh
@@ -0,0 +1,12 @@
+cryptsetup open /dev/nvme0n1p2 luks
+mount -o noatime,nodiratime,compress=lzo,space_cache=v2,ssd,subvol=@ /dev/mapper/luks /mnt
+mkdir -p /mnt/{boot,home,var/cache/pacman/pkg,var/log,.snapshots,btrfs}
+mount -o noatime,nodiratime,compress=lzo,space_cache=v2,ssd,subvol=@home /dev/mapper/luks /mnt/home
+mount -o noatime,nodiratime,compress=lzo,space_cache=v2,ssd,subvol=@pkg /dev/mapper/luks /mnt/var/cache/pacman/pkg
+mount -o noatime,nodiratime,compress=lzo,space_cache=v2,ssd,subvol=@var_log /dev/mapper/luks /mnt/var/log
+mount -o noatime,nodiratime,compress=lzo,space_cache=v2,ssd,subvol=@snapshots /dev/mapper/luks /mnt/.snapshots
+mount -o noatime,nodiratime,compress=lzo,space_cache=v2,ssd,subvolid=5 /dev/mapper/luks /mnt/btrfs
+mount /dev/nvme0n1p1 /mnt/boot
+arch-chroot /mnt/
+pacman -Syyu linux linux-headers
+mkinitcpio -P
